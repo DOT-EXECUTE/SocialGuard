9ca8027d3ed638a109ec468cee07d1b669450726
DOT-EXECUTE <dotexecute853@gmail.com>
2026-09-15 16:27:45 +1000

    Chore: Init

---
+++ social_block.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+HOSTS_FILE="/etc/hosts"
+BLOCKLIST="$HOME/.blocklist"
+MARKER="# SOCIAL_MEDIA_BLOCK"
+
+block() {
+  checkBlocked # Check wether or not we've already blocked the website
+
+  sudo cp "$HOSTS_FILE" "$HOSTS_FILE.bak"
+  echo "$MARKER START" | sudo tee -a "$HOSTS_FILE" >/dev/null
+  while read -r domain; do
+    if [[ $domain != "# ~/.blocklist" ]]; then
+      echo "127.0.0.1 $domain" | sudo tee -a "$HOSTS_FILE" >/dev/null
+    fi
+  done <"$BLOCKLIST"
+  echo "$MARKER END" | sudo tee -a "$HOSTS_FILE" >/dev/null
+  echo "[✓] Sites blocked."
+}
+
+unblock() {
+  sudo cp "$HOSTS_FILE" "$HOSTS_FILE.bak"
+  sudo sed -i.bak "/$MARKER START/,/$MARKER END/d" "$HOSTS_FILE"
+  echo "[✓] Sites unblocked."
+}
+
+checkBlocked() {
+  if grep -i -q "$MARKER START" /etc/hosts; then
+    echo "Error: Socials are already blocked! You must unblock with 'unblock'"
+    exit 1
+  fi
+}
+
+case "$1" in
+block)
+  block
+  ;;
+unblock)
+  unblock
+  ;;
+*)
+  echo "Usage: $0 [block|unblock]"
+  exit 1
+  ;;
+esac
